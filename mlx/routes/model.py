5b23588354a7d6b0bfcdeced201553c5e2d8e8fe
otomomik <developer@otomomik.com>
2025-03-20 19:02:33 +0900

    feat(api): add get model route

--- mlx/routes/model.py
+++ mlx/routes/model.py
@@ -1,14 +1,19 @@
 import json
 import importlib
-from typing import Dict, List, Optional, Union
-from fastapi import APIRouter
+import logging
+from typing import List, Optional
+from fastapi import APIRouter, HTTPException
 from huggingface_hub import HFCacheInfo, scan_cache_dir
+from mlx_lm.utils import MODEL_REMAPPING as LM_MODEL_REMAPPING
+from mlx_vlm.utils import MODEL_REMAPPING as VL_MODEL_REMAPPING
 
 from schemas.model import Model
+
 
 router = APIRouter()
 
 cache_dir: Optional[HFCacheInfo] = None
+cache_models: Optional[List[Model]] = None
 
 
 def get_cache_dir():
@@ -18,15 +23,34 @@ def get_cache_dir():
     return cache_dir
 
 
-@router.get("/")
-def chat_completions():
-    supported_models: List[Model] = []
+def _get_models():
+    global cache_models
+    if cache_models is None:
+        cache_models = []
+        for repo in get_cache_dir().repos:
+            model = _get_model(repo.repo_id)
 
+            if model is not None:
+                cache_models.append(model)
+
+    return cache_models
+
+
+def _get_model(model_id: str):
     for repo in get_cache_dir().repos:
-        if repo.repo_type != "model":
+        id = repo.repo_id
+        type = repo.repo_type
+        if model_id != repo.repo_id:
             continue
 
-        first_revision = next(iter(repo.revisions), None)
+        if "mlx" not in id:
+            continue
+
+        if type != "model":
+            continue
+
+        revisions = repo.revisions
+        first_revision = next(iter(revisions), None)
         if not first_revision:
             continue
 
@@ -36,34 +60,65 @@ def chat_completions():
         if not config_file:
             continue
 
-        # try:
-        #     with open(config_file.file_path, "r") as f:
-        #         config_data = json.load(f)
-        # except Exception:
-        #     continue
+        try:
+            with open(config_file.file_path, "r") as f:
+                config_data = json.load(f)
+        except Exception:
+            continue
 
-        # is_text = False
-        # try:
-        #     importlib.import_module(f"mlx_lm.models.{config_data['model_type']}")
-        #     is_text = True
-        # except Exception:
-        #     pass
+        text_to_text = False
+        image_to_text = False
+        video_to_text = False
+        model_type = config_data.get("model_type")
 
-        # is_image = False
-        # try:
-        #     importlib.import_module(f"mlx_vlm.models.{config_data['model_type']}")
-        #     is_image = True
-        # except Exception:
-        #     pass
+        text_model_type = LM_MODEL_REMAPPING.get(model_type, model_type)
+        try:
+            importlib.import_module(f"mlx_lm.models.{text_model_type}")
+            text_to_text = True
+        except Exception:
+            logging.debug(f"Model type {text_model_type} not text_to_text supported.")
 
-        supported_models.append(
-            Model(
-                id=repo.repo_id,
-                created=int(repo.last_modified),
-                owned_by=repo.repo_id.split("/")[0]
-                if "/" in repo.repo_id
-                else repo.repo_id,
+        image_model_type = VL_MODEL_REMAPPING.get(model_type, model_type)
+        try:
+            importlib.import_module(f"mlx_vlm.models.{image_model_type}")
+            text_to_text = True
+            image_to_text = True
+        except Exception:
+            logging.debug(f"Model type {image_model_type} not image_to_text supported.")
+
+        try:
+            video_to_text = hasattr(config_data, "video_token_id") or hasattr(
+                config_data, "video_token_index"
             )
+            if video_to_text:
+                text_to_text = True
+        except Exception:
+            logging.debug(f"Model type {image_model_type} not video_to_text supported.")
+
+        last_modified = repo.last_modified
+        return Model(
+            id=id,
+            created=int(last_modified),
+            owned_by=id.split("/")[0] if "/" in id else id,
+            text_to_text=text_to_text,
+            image_to_text=image_to_text,
+            video_to_text=video_to_text,
         )
 
-    return {"object": "list", "data": supported_models}
+    return None
+
+
+@router.get("/")
+def get_models():
+    models = _get_models()
+    return {"object": "list", "data": models}
+
+
+@router.get("/{model_id:path}")
+def get_model(model_id: str):
+    models = _get_models()
+    for model in models:
+        if model.id == model_id:
+            return model
+
+    raise HTTPException(status_code=404, detail="Model not found")
--- mlx/schemas/model.py
+++ mlx/schemas/model.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseModel
 
 
@@ -6,3 +7,6 @@ class Model(BaseModel):
     object: str = "model"
     created: int
     owned_by: str
+    text_to_text: bool
+    image_to_text: bool
+    video_to_text: bool
