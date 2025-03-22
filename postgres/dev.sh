fbeb2cba9a2081015e4f81420e0cf640dd323285
otomomik <developer@otomomik.com>
2025-03-22 16:05:27 +0900

    infra(postgres): check docker build

--- postgres/dev.sh
+++ postgres/dev.sh
@@ -2,5 +2,5 @@
 
 CONTAINER_NAME=local_ai_workspace_postgres
 
-docker build -t "${CONTAINER_NAME}" .
+docker image inspect "${CONTAINER_NAME}" >/dev/null 2>&1 && echo "Container ${CONTAINER_NAME} already exists" || docker build -t "${CONTAINER_NAME}" .
 docker run --rm -p "$POSTGRES_PORT":5432 --name "${CONTAINER_NAME}" -v ./data:/var/lib/postgresql/data -e POSTGRES_USER="$POSTGRES_USER" -e POSTGRES_PASSWORD="$POSTGRES_PASSWORD" -e POSTGRES_DB="$POSTGRES_DB" -e PGDATA="$PGDATA" "${CONTAINER_NAME}"
